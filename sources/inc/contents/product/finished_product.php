d31ad6a65b4701ee81bc1bf56374cefb922fa447
Hafijul Islam <hafijul233@gmail.com>
2020-10-05 19:14:14 +0600

    product section complete

--- sources/inc/contents/product/finished_product.php
+++ sources/inc/contents/product/finished_product.php
@@ -2,13 +2,14 @@
 <br/>
 <?php
 include("sources/inc/double_date_id.php");
-$info = $qur->get_finished_product_overview($date1, $date2);
-$n = count($info);
+$products = $qur->get_finished_product_overview($date1, $date2);
+$n = count($products);
 if ($n > 0) {
     $qun = 0;
     $cos = 0;
     echo "<br/><a id='printBox' href='print.php?e=" . $encptid . "&page=product&&sub=finished_product&&date1=" . $date1 . "&&date2=" . $date2 . "' class='button' target='_blank'><b> Print </b></a><br/>";
-    echo "<table align='center' class='rb'>";
+    echo "<table align='center' class='rb table'>";
+    echo "<thead>";
     echo "<tr>";
     echo "<td>";
     echo "Date";
@@ -29,31 +30,35 @@ if ($n > 0) {
     echo "Rate";
     echo "</td>";
     echo "</tr>";
-    foreach ($info as $i) {
+    echo "</thead>";
+    echo "<tbody>";
+    foreach ($products as $finished) {
         echo "<tr>";
         echo "<td>";
-        echo $inp->date_convert($i[0]);
+        echo $inp->date_convert($finished[0]);
         echo "</td>";
 
         echo "<td>";
-        echo $i[1];
+        echo isset($finished[1]) ? $finished[1] : '-';
         echo "</td>";
 
         echo "<td>";
-        echo $i[2];
+        echo isset($finished[2]) ? $finished[2] : '-';
         echo "</td>";
         echo "<td>";
-        echo $i[3];
+        echo isset($finished[3]) ? $finished[3] : '-';
         echo "</td>";
         echo "<td>";
-        echo $i[4];
+        echo isset($finished[4]) ? $finished[4] : '-';
         echo "</td>";
         echo "<td>";
-        echo $i[5];
+        echo isset($finished[5]) ? $finished[5] : '-';
         echo "</td>";
 
         echo "</tr>";
     }
+    echo "</tbody>";
+    /*
     echo "<tr>";
     echo "<td colspan = 2>";
     echo "Total ";
@@ -65,6 +70,7 @@ if ($n > 0) {
     echo "<td><b>" . $cos . "</b></td>";
     echo "<td> </td>";
     echo "</tr>";
+    */
     echo "</table>";
     echo "<br/><a id='printBox' href='print.php?e=" . $encptid . "&page=product&&sub=finished_product&&date1=" . $date1 . "&&date2=" . $date2 . "' class='button' target='_blank'><b> Print </b></a>";
 } else {
