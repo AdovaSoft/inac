8b24c53d841ab8dbf59a2af8a9e97db3623df195
Shiftgithub <sajibadhikary@gmail.com>
2020-10-05 06:19:36 -0700

    particular product

--- sources/inc/contents/product/particular_product.php
+++ sources/inc/contents/product/particular_product.php
@@ -26,7 +26,8 @@ if (isset($id)) {
 
         echo "<h3 class='green'>Current Stock : " . $qur->current_stock($id) . "</h3>";
 
-        echo "<br/><table align='center' class='rb'>";
+        echo "<br/><table align='center' class='rb table'>";
+        echo "<thead>";
         echo "<tr>";
         echo "<td>";
         echo "Date";
@@ -44,23 +45,25 @@ if (isset($id)) {
         echo "Total";
         echo "</td>";
         echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
         foreach ($info as $i) {
             echo "<tr>";
             echo "<td>";
-            echo $inp->date_convert($i[0]);
+            echo isset($i[0]) ? $inp->date_convert($i[0]) : '-';
             echo "</td>";
 
             echo "<td>";
-            echo $i[1];
+            echo isset($i[1]) ? $i[1] : '-';
             echo "</td>";
 
             echo "<td>";
-            echo $i[2];
+            echo isset($i[2])? $i[2] : '-';
             $qun += $i[2];
             echo "</td>";
 
             echo "<td>";
-            echo money($i[3]);
+            echo isset($i[3]) ? money($i[3]) : '-';
 
             echo "</td>";
 
@@ -70,6 +73,8 @@ if (isset($id)) {
             echo "</td>";
             echo "</tr>";
         }
+        echo "</tbody>";
+        echo "<tfoot>";
         echo "<tr>";
         echo "<td colspan = 2>";
         echo "Total ";
@@ -80,6 +85,7 @@ if (isset($id)) {
         echo "<td> </td>";
         echo "<td><b>" . money($cos) . "</b></td>";
         echo "</tr>";
+        echo "</tfoot>";
         echo "</table>";
         echo "<br/><a id='printBox' href='print.php?e=" . $encptid . "&page=product&&sub=particular_product&&date1=" . $date1 . "&&date2=" . $date2 . "&&id=" . $id . "' class='button' target='_blank'><b> Print </b></a>";
     } else {
