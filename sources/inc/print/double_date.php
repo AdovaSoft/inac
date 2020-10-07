9ed6c574080ce73ce6c6a7e7bb63b8192e9c9054
Hafijul Islam <hafijul233@gmail.com>
2020-10-08 01:29:25 +0600

    date code added

--- sources/inc/print/double_date.php
+++ sources/inc/print/double_date.php
@@ -1,6 +1,6 @@
 <?php
-$date1 = $_GET['date1'];
-$date2 = $_GET['date2'];
+$date1 = (isset($_GET['date1'])) ? $_GET['date1'] : date('Y-m-d');
+$date2 = (isset($_GET['date2'])) ? $_GET['date2'] : date('-30days', strtotime($date1));
 if (isset($date1) && isset($date2))
     echo "<h3>Report from " . $inp->date_convert($date1) . " to " . $inp->date_convert($date2) . "</h3>";
 
