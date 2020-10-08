0fda305dadeaa20df2cce42a341ad0f13c6176dc
robinNcode <msmrobin518@gmail.com>
2020-10-08 16:24:21 +0600

    print/individual_purchase.php

--- sources/inc/print/party/individual_purchase.php
+++ sources/inc/print/party/individual_purchase.php
@@ -1,5 +1,5 @@
 <?php
-$id = $_GET[id];
+$id = $_GET['id'];
 include("sources/inc/print/double_date.php");
 if ($date1 && $date2) {
     $query2 = sprintf("SELECT date, SUM(unite*rate) as purchase,discount FROM (SELECT date, idpurchase FROM purchase WHERE idparty = %d AND date BETWEEN '%s' AND '%s' ) as purchase LEFT JOIN purchase_details USING (idpurchase)  LEFT JOIN purchase_discount USING(idpurchase)  GROUP BY idpurchase;", $id, $date1, $date2);
@@ -9,6 +9,7 @@ if ($date1 && $date2) {
 }
 $name = $qur->get_cond_row('party', array('name'), 'idparty', '=', $id);
 $pur = $qur->get_custom_select_query($query2, 3);
+$r_bill_d = $r_bill_t = $total = 0;
 if (count($pur) > 0) {
     echo "<h4>Purchased from " . $name[0][0] . ": </h4>";
     echo "<table align='center' class='rb'>";
@@ -27,13 +28,14 @@ if (count($pur) > 0) {
     echo "</tr>";
     foreach ($pur as $s) {
         echo "<tr>";
-        echo "<td>" . $inp->date_convert($s[0]) . "</td>" . "<td >" . sprintf("%.2f", $s[1]) . "</td>" . "<td>" . $s[2] . "</td>";
+        echo "<td>" . $inp->date_convert($s[0]) . "</td>" . "<td >" . money($s[1]) . "</td>" . "<td>" . money($s[2]) . "</td>";
         $r_bill_t += $s[1];
         $r_bill_d += $s[2];
         echo "</tr>";
     }
-    echo "<tr><td>Sum </td> <td>" . sprintf("%.2f", $r_bill_t) . "</td><td>" . sprintf("%.2f", $r_bill_d) . "</td></tr>";
-    echo "<tr><td> Grand Total  </td> <td colspan = '2' > <b>" . sprintf("%.2f", $r_bill_t - $r_bill_d) . "</b></td></td></tr>";
+    echo "<tr><td>Sum </td> <td>" . money($r_bill_t) . "</td><td>" . money($r_bill_d) . "</td></tr>";
+    $g_total = $r_bill_t - $r_bill_d;
+    echo "<tr><td> Grand Total  </td> <td colspan = '2' > <b>" .money($g_total) . "</b></td></td></tr>";
     echo "</table>";
     $total = $qur->party_adv_due($id);
     if ($total < 0) {
