32f19131d12b90481ba8bc2eb6d48aa54064e2e3
Shiftgithub <sajibadhikary@gmail.com>
2020-10-11 20:40:51 +0600

    factory_finished print

--- sources/inc/print/product/particular_product.php
+++ sources/inc/print/product/particular_product.php
@@ -10,24 +10,37 @@ if ($n > 0) {
     echo "<h3>Current Stock : " . $qur->current_stock($id) . "</h3>";
     echo "<br/><table align='center' class='rb'>";
     echo "<tr>";
+
+    echo "<th>SI</th>";
+
     echo "<th>";
     echo "Date";
     echo "</th>";
+    
     echo "<th>";
     echo "Party";
     echo "</th>";
+    
     echo "<th>";
     echo "Quantity";
     echo "</th>";
+    
     echo "<th>";
     echo "Price";
     echo "</th>";
+    
     echo "<th>";
     echo "Total";
     echo "</th>";
+    
     echo "</tr>";
+
+    $j = 0;
     foreach ($info as $i) {
         echo "<tr>";
+
+        echo "<td>" . $j++ . "</td>";
+
         echo "<td>";
         echo $inp->date_convert($i[0]);
         echo "</td>";
@@ -38,15 +51,14 @@ if ($n > 0) {
 
         echo "<td>";
         echo esc($i[2]);
+        echo "</td>";
         $qun += $i[2];
-        echo "</td>";
 
-        echo "<td>";
+        echo "<td class='text-right' >";
         echo money($i[3]);
-
         echo "</td>";
 
-        echo "<td>";
+        echo "<td class='text-right' >";
         $total = $i[2] * $i[3];
         echo money($total);
         $cos += $i[2] * $i[3];
@@ -54,14 +66,13 @@ if ($n > 0) {
         echo "</tr>";
     }
     echo "<tr>";
-    echo "<td colspan = 2>";
+    echo "<th colspan = 3>";
     echo "Total ";
-    echo "</td>";
+    echo "</th>";
     echo "<td ><b>";
     echo $qun;
     echo "</b></td>";
-    echo "<td> </td>";
-    echo "<td><b>" . money($cos) . "</b></td>";
+    echo "<td colspan='2'><b>" . money($cos) . "</b></td>";
     echo "</tr>";
     echo "</table>";
 }
