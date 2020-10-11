9f83d9d364763dc5c974034ea3e41c754200a046
Shiftgithub <sajibadhikary@gmail.com>
2020-10-11 18:23:49 +0600

    daily report

--- sources/inc/print/stock/daily_report.php
+++ sources/inc/print/stock/daily_report.php
@@ -11,42 +11,48 @@ if ($n > 0) {
     echo "<small>Report according to date " . date("d M Y (D)") . "</small><br/>";
     echo "<table align='center' class='rb'>";
     echo "<tr>";
-    echo "<td>";
+
+    echo "<th>SI</th>";
+
+    echo "<th>";
     echo "Date";
-    echo "</td>";
+    echo "</th>";
 
-    echo "<td>";
+    echo "<th>";
     echo "Product";
-    echo "</td>";
+    echo "</th>";
 
-    echo "<td>";
+    echo "<th>";
     echo "Price (TK)";
-    echo "</td>";
+    echo "</th>";
 
 
-    echo "<td>";
+    echo "<th>";
     echo "Incoming";
-    echo "</td>";
+    echo "</th>";
 
-    echo "<td>";
+    echo "<th>";
     echo "Outgoing";
-    echo "</td>";
+    echo "</th>";
 
-    echo "<td>";
+    echo "<th>";
     echo "Unit";
-    echo "</td>";
+    echo "</th>";
 
-    echo "<td>";
+    echo "<th>";
     echo "Total Price";
-    echo "</td>";
+    echo "</th>";
 
-    echo "<td>";
+    echo "<th>";
     echo "Remark";
-    echo "</td>";
+    echo "</th>";
     echo "</tr>";
 
+    $j = 0;
     foreach ($info as $i) {
         echo "<tr>";
+
+        echo "<td>" . $j++ . "</td>";
 
         echo "<td>";
         echo $inp->date_convert($i[0]);
@@ -56,36 +62,44 @@ if ($n > 0) {
         echo $i[1];
         echo "</td>";
 
-        echo "<td>";
+        echo "<td class='text-right'>";
         echo money($i[4]);
         echo "</td>";
+
         $tti_p = 0;
         if ($i[2] > 0) {
-            echo "<td>";
+
+            echo "<td class='text-right'>";
+            echo money($i[2]);
+            echo "</td>";
+            if ($i[5] == 0 || $i[5] == 1) $tti_p = $tti_p + $i[2];
+
+            if ($i[5] == 0 || $i[5] == 1) {
+                echo "<td>";
+                echo "-";
+                echo "</td>";
+            } else {
+                echo "<td class='text-right'>";
+                echo money($i[2]);
+                echo "</td>";
+            }
+        } else {
+            
+            if ($i[5] == 0 || $i[5] == 1) {
+                echo "<td>";
+                echo "-";
+                echo "</td>";
+            } else {
+                echo "<td class='text-right'>";
+                echo money($i[2]);
+                echo "</td>";
+            }
+            
+            echo "<td class='text-right' >";
+            $i[2] = -$i[2];
             echo money($i[2]);
             if ($i[5] == 0 || $i[5] == 1)
-                $tti_p = $tti_p + $i[2];
-
-            echo "</td>";
-            echo "<td>";
-            if ($i[5] == 0 || $i[5] == 1) {
-                echo "-";
-            } else {
-                echo money($i[2]);
-            }
-            echo "</td>";
-        } else {
-            echo "<td>";
-            if ($i[5] == 0 || $i[5] == 1) {
-                echo "-";
-            } else {
-                echo $i[2];
-            }
-            echo "</td>";
-            echo "<td>";
-            echo(-$i[2]);
-            if ($i[5] == 0 || $i[5] == 1)
-                $tto_p = $tto_o + (-$i[2]);
+                $tto_p = $tto_o  - $i[2];
             echo "</td>";
         }
 
@@ -123,7 +137,6 @@ if ($n > 0) {
     $tto *= (-1);
     echo "<tr><th colspan='3'>Total Incoming : " . money($tti) . " TK</th><th colspan='3'>Total Outgoing : " . money($tto) . " TK</th><th colspan='2'>Total (Incoming  -  Outgoing) : <br/>" . money($total) . " TK</th></tr>";
     echo "</table>";
-    
 } else {
     echo "<br/><h2 class='blue'>No input or output in " . $inp->date_convert($date) . " date</h2>";
 }
